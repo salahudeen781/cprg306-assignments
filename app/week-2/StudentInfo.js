c39836228713e6fd0537f7cfc5ffe9df11663cf6
salahudeen781 <61598552+salahudeen781@users.noreply.github.com>
2024-09-27 10:25:30 -0700

    week-2
    Assignments

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Salahudeen Ansari</h1>
+      <Link href="https://github.com/salahudeen781/salahudeenansari">
+        "https://github.com/salahudeen781/salahudeenansari"
+      </Link>
+    </main>
+  );
+}
